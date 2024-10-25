fb7d5ac95745319dd223ead092910ca8d123b2f9
hi-jennie <jennie.wang2024@gmail.com>
2024-10-25 10:03:19 +0800

    Refactor checkout.js to render delivery options dynamically

--- data/cart.js
+++ data/cart.js
@@ -1,3 +1,5 @@
+import { deliveryOptions } from "./deliveryOption.js";
+
 // JSON.parse(localStorage.getItem('cart')); 是从本地解析一个叫'cart'的东西。
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
@@ -5,11 +7,13 @@ if(!cart){
   cart = [
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '1'
   },
   {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
   }
 ];
 }
@@ -33,9 +37,10 @@ export function addToCart(productId){
   if(matchingItem){
     matchingItem.quantity += 1;
   }else{
-    carts.push({
+    cart.push({
       productId: productId,
-      quantity: 1
+      quantity: 1,
+      deliveryOptionId: '1'
     });
   }
   saveToStorage();
@@ -50,5 +55,18 @@ export function removeFromCart(productId){
   });
   
   cart = newCart;
+  saveToStorage();
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId){
+  let matchingItem;
+
+  cart.forEach((item)=>{
+    if(item.productId === productId){
+      matchingItem = item;
+    }
+  });
+
+  matchingItem.deliveryOptionId = deliveryOptionId;
   saveToStorage();
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,19 @@
-import {cart,removeFromCart} from '../data/cart.js';
+import {cart,removeFromCart, updateDeliveryOption} from '../data/cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
+import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import {deliveryOptions} from '../data/deliveryOption.js';
 
+/*
+hello();
+const today = dayjs();
+const deliveryDate = today.add(7, 'days');
+console.log(deliveryDate.format('dddd, MMMM D'));
+*/
+
+function renderOrderSummery(){
 // the final HTML
-console.log(cart);
 let cartSummeryHTML = '';
 
 // generate the HTML for every cart item
@@ -16,9 +26,23 @@ cart.forEach((cartItem)=>{
       matchingProduct = product;
     }
   });
+
+  let deliveryOptionId = cartItem.deliveryOptionId;
+  let matchingDeliveryOption;
+
+  deliveryOptions.forEach((curOption)=>{
+    if(curOption.id===deliveryOptionId){
+      matchingDeliveryOption = curOption;
+    }
+  })
+
+  const today = dayjs();
+  const shippingDays = today.add(`${matchingDeliveryOption.deliveryDays}`,'days').format('dddd, MMMM D');
+  
+
   cartSummeryHTML += `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
-        Delivery date: Tuesday, June 21
+        Delivery date: ${shippingDays}
       </div>
 
       <div class="cart-item-details-grid">
@@ -49,50 +73,44 @@ cart.forEach((cartItem)=>{
           <div class="delivery-options-title">
             Choose a delivery option:
           </div>
-          <div class="delivery-option">
-            <input type="radio" checked
-              class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Tuesday, June 21
-              </div>
-              <div class="delivery-option-price">
-                FREE Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Wednesday, June 15
-              </div>
-              <div class="delivery-option-price">
-                $4.99 - Shipping
-              </div>
-            </div>
-          </div>
-          <div class="delivery-option">
-            <input type="radio"
-              class="delivery-option-input"
-              name="delivery-option-${matchingProduct.id}">
-            <div>
-              <div class="delivery-option-date">
-                Monday, June 13
-              </div>
-              <div class="delivery-option-price">
-                $9.99 - Shipping
-              </div>
-            </div>
-          </div>
+          ${deliveryOptionHTML(matchingProduct,cartItem)}
         </div>
       </div>
     </div>`;
 
 });
+
+function deliveryOptionHTML(matchingProduct,cartItem){
+  let html = '';
+
+  deliveryOptions.forEach((deliverOption)=>{
+    const today = dayjs();
+
+    const shippingDays = today.add(`${deliverOption.deliveryDays}`,'days').format('dddd, MMMM D');
+
+    const deliveryFees = deliverOption.id === '1' ? 'FREE' : `$${formatCurrency(deliverOption.priceCents)}`;
+    
+    const isChecked = deliverOption.id === cartItem.deliveryOptionId;
+    html += `AZ/?????????????????????????????????????????????????
+    <div class="delivery-option js-delivery-option" 
+      data-product-id = ${matchingProduct.id} data-delivery-option-id = ${deliverOption.id}>
+      <input type="radio" 
+        ${isChecked ? 'checked' : ''}
+        class="delivery-option-input"
+        name="delivery-option-${matchingProduct.id}">
+      <div>
+        <div class="delivery-option-date">
+          ${shippingDays}
+        </div>
+        <div class="delivery-option-price">
+          ${deliveryFees } Shipping
+        </div>
+      </div>
+    </div>
+    `;
+  });
+  return html;
+}
 
 document.querySelector('.js-cart-summery').innerHTML = cartSummeryHTML;
 
@@ -106,6 +124,19 @@ document.querySelectorAll('.js-delete-link')
       const deleteContainer = document.querySelector(`.js-cart-item-container-${productId}`);
       deleteContainer.remove();
       
-    });
+    });   
+  });
+
+
+document.querySelectorAll('.js-delivery-option')
+  .forEach((element)=>{
+    element.addEventListener('click',()=>{
+      const {productId, deliveryOptionId} = element.dataset;
+      updateDeliveryOption(productId, deliveryOptionId);
+      renderOrderSummery();
+    })
     
   });
+}
+
+renderOrderSummery();
