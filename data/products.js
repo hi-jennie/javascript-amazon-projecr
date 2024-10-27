57a9fa6179eab7c8d6d28a596b887dda907b0b04
hi-jennie <jennie.wang2024@gmail.com>
2024-10-27 11:18:27 +0800

    convert products into class

--- data/products.js
+++ data/products.js
@@ -1,3 +1,4 @@
+import { formatCurrency } from "../scripts/utils/money.js";
 export function getProductById(productId) {
   let matchingProduct;
   products.forEach((currProduct) => {
@@ -21,23 +22,15 @@ class Product {
     this.rating = productDetails.rating;
     this.priceCents = productDetails.priceCents;
   }
+
+  getStarUrl(){
+    return `images/ratings/rating-${(this.rating.stars)*10}.png`;
+  }
+
+  getPrice(){
+    return `$${formatCurrency(this.priceCents)}`;
+  }
 }
-
-const product1 = new Product({id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-  name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090,
-  keywords: [
-    "socks",
-    "sports",
-    "apparel"
-  ]});
-
-  console.log(product1);
 
 
 export const products = [
