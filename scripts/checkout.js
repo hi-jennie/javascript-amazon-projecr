2e392227fa06596d9ed680f0fd3a9fdc2f065e56
hi-jennie <jennie.wang2024@gmail.com>
2024-10-27 23:19:34 +0800

    async and await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,8 +4,33 @@ import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 import '../data/cart-class.js';
 
-// Promise.all let us to run all the promises at the same time
+// async: this will return a promise
 
+async function loadPage(){
+  console.log('loadPage');
+  // await: block the code until the promise is resolved
+  await loadProductsFetch();
+
+  // instead of pass value3 to the next then(), we can use a variable to store the value
+  const value = await new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve('value3');
+    });
+  });
+
+  renderOrderSummery();
+  renderPaymentSummery();
+
+  return 'value1'; // this will be saved as the argument of the next then()
+}
+
+loadPage().then((value)=>{
+  console.log(value);
+});
+
+
+/*
+// Promise.all let us to run all the promises at the same time
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve)=>{
@@ -18,6 +43,8 @@ Promise.all([
   renderOrderSummery();
   renderPaymentSummery();
 });
+*/
+
 
 /*
 // Promise is better than callback, because it can keep the code flat and easy to read
